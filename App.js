a00eed305867709f213fd075c9477fdb9069786f
Vikas v <vikasmyya@gmail.com>
2025-07-24 10:39:02 +0530

    Added functional component

--- App.js
+++ App.js
@@ -4,8 +4,24 @@ import ReactDOM from "react-dom/client";
 //JSX
 const jsxHeading = <h1> React from JSX</h1>;
 
+//Functional complonent
+const HeadingComponent = () => {
+  return <h1>Hellp from component1</h1>;
+};
+
+//short hand
+const HeadingComponent3 = () => <h1>Hello from complonent3</h1>;
+
+//component composition
+const HeadingComponent2 = () => (
+  <div>
+    <HeadingComponent />
+    <h1>Hello from component2</h1>
+  </div>
+);
+
 console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent2 />);
