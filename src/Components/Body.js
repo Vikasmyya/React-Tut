380aa503bd0e3dfc5bfc865806f7654d6ad45841
Vikas v <vikasmyya@gmail.com>
2025-07-24 19:20:10 +0530

    Added useEffect and Shimmer UI

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,29 @@
 import RestaurentCard from "./RestaurentCard";
-import resList from "../Utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmmer from "./Shimmer";
 
 const Body = () => {
-  const [listofRes, setlistofRes] = useState(resList);
+  const [listofRes, setlistofRes] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0488712&lng=77.5741212&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    setlistofRes(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listofRes.length === 0) {
+    return <Shimmmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
---
+++ src/Components/Shimmer.js
@@ -0,0 +1,17 @@
+const Shimmmer = () => {
+  return (
+    <div className="shimmer">
+      <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmmer;
