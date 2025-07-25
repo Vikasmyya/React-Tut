714612f91057c73cbd2d97810766b15c9b222d21
Vikas v <vikasmyya@gmail.com>
2025-07-25 15:46:11 +0530

    Added state variable in class component

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -3,10 +3,27 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+    };
   }
   render() {
+    const { name } = this.props;
+    const { count } = this.state;
+
     return (
       <div className="user">
+        <h1>Count: {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1,
+            });
+          }}
+        >
+          Count Increment
+        </button>
         <h2>Name: {this.props.name}</h2>
         <p>Location: Karnataka</p>
         <p>Contact: vikas@gmail.com</p>
