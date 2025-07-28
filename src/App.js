5e08b9371d02e091ca1d1b6d2977fc224a7158f3
Vikas v <vikasmyya@gmail.com>
2025-07-28 15:27:50 +0530

    Added lazy loading and suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./Components/About";
-import Contact from "./Components/Contact";
 import Error from "./Components/Error";
 import RestaurentMenu from "./Components/RestaurentMenu";
+
+const Contact = lazy(() => {
+  return import("./Components/Contact");
+});
 
 const AppLayout = () => {
   return (
@@ -32,7 +35,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/contact",
-        element: <Contact />,
+        element: (
+          <Suspense fallback={<h2>Loading</h2>}>
+            <Contact />
+          </Suspense>
+        ),
       },
       {
         path: "/restaurent/:resId",
