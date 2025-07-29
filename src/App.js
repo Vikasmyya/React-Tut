ec80c1501ac370299b1d4f27358fb2f56dc3ea18
Vikas v <vikasmyya@gmail.com>
2025-07-29 16:07:25 +0530

    Added redux and configured store and slice

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import About from "./Components/About";
 import Error from "./Components/Error";
 import RestaurentMenu from "./Components/RestaurentMenu";
 import UserContext from "./Utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./Utils/appStore";
 
 const Contact = lazy(() => {
   return import("./Components/Contact");
@@ -24,14 +26,16 @@ const AppLayout = () => {
   }, []);
 
   return (
-    <UserContext.Provider
-      value={{ loggedinUser: userName, setuserName, userName }}
-    >
-      <div className="app">
-        <Header />
-        <Outlet />
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider
+        value={{ loggedinUser: userName, setuserName, userName }}
+      >
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 
